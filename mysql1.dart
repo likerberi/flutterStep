12ebba70bfa9c00e30a61b662a1267864b8d39d2
seungyongC <likerberi97@gmail.com>
2020-03-28 18:54:08 +0900

    sampling for utility

--- mysql1.dart
+++ mysql1.dart
@@ -12,5 +12,25 @@ Future main() async {
     db: 'db',
   ));
 
-  runApp(MyApp(conn));
+  products = await conn.query('select * from products');
+     
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'MySQL sampling',
+      darkTheme: ThemeData.dark(),
+      home: HomePage(),
+    );
+  }
+}
+
+Widget getTextWidgets(List<String> strings, index) {
+  String temp = jsonEncode(products.toString());
+  List<String> preferenceChoices = temp.split("Fields");
+  
+  return Column(children: strings.map((item) => new Text(item)).toList().sublist(index++)); 
 }
