40f6061dabea12179d52faf20bb659ff522bbb60
seungyongC <likerberi97@gmail.com>
2021-02-23 15:59:36 +0900

    simple login form

---
+++ login_form/login_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _LoginPageState();
+  }
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      minimum: EdgeInsets.symmetric(horizontal: 40),
+      child: Stack(
+        children: [
+          _loginForm(),
+          Container(
+            alignment: Alignment.bottomCenter,
+            child: FlatButton(
+                onPressed: () {},
+                child: Text('Don\'t have an account? Sign up.')),
+          )
+        ],
+      ),
+    ));
+  }
+
+  Widget _loginForm() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        TextField(
+          controller: _usernameController,
+          decoration:
+              InputDecoration(icon: Icon(Icons.mail), labelText: 'Username'),
+        ),
+        TextField(
+          controller: _passwordController,
+          decoration: InputDecoration(
+              icon: Icon(Icons.lock_open), labelText: 'Password'),
+          obscureText: true,
+          keyboardType: TextInputType.visiblePassword,
+        ),
+        FlatButton(
+          onPressed: _login,
+          child: Text('Login'),
+          color: Theme.of(context).accentColor,
+        )
+      ],
+    );
+  }
+
+  void _login() {
+    final username = _usernameController.text.trim();
+    final password = _passwordController.text.trim();
+
+    print('username: $username');
+    print('password: $password');
+  }
+}
---
+++ login_form/main.dart
@@ -0,0 +1,32 @@
+import 'package:amplify/login_page.dart';
+import 'package:flutter/material.dart';
+import 'login_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Cracy',
+        theme: ThemeData(
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: Navigator(
+          pages: [
+            MaterialPage(child: LoginPage()),
+          ],
+          onPopPage: (route, result) => route.didPop(result),
+        ));
+  }
+}
