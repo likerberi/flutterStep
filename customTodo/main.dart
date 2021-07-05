76fa436b5b6aad6f19995706fc81054c3ec6be8e
seungyongC <likerberi97@gmail.com>
2021-07-05 15:37:40 +0900

    customTodo

---
+++ customTodo/main.dart
@@ -0,0 +1,62 @@
+import 'package:fl_live_launcher/apps.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      child: MaterialApp(
+        title: 'Flutter Launcher',
+        darkTheme: ThemeData.dark().copyWith(
+          primaryColor: Colors.red,
+          accentColor: Colors.red,
+        ),
+        theme: ThemeData(
+          primarySwatch: Colors.red,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        onGenerateRoute: (settings) {
+          return MaterialPageRoute(
+              settings: settings,
+              builder: (context) {
+                switch (settings.name) {
+                  case "apps":
+                    return AppsPage();
+                  default:
+                    return HomePage();
+                }
+              });
+        },
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.transparent,
+        elevation: 0,
+        child: Container(
+          height: 70,
+          child: Center(
+            child: IconButton(
+              icon: Icon(Icons.apps),
+              onPressed: () => Navigator.pushNamed(context, "apps"),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
