660b29fd67ea5edab3c21596f562aa966ea2546f
S.Y.C <likerberi97@gmail.com>
2023-02-21 16:20:44 +0900

    updated using flutter_riverpod
    thx chatGPT

--- decodingFlutter/chatGPT.dart
+++ decodingFlutter/chatGPT.dart
@@ -1,20 +1,25 @@
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
 
-bool useIsFocused(FocusNode node) {
-  final isFocused = useState(node.hasFocus);
+final focusNodeProvider = Provider.autoDispose<FocusNode>((ref) {
+  final focusNode = FocusNode();
+  ref.onDispose(() => focusNode.dispose());
+  return focusNode;
+});
 
-  useEffect(
-        () {
-      void listener() {
-        isFocused.value = node.hasFocus;
-      }
+final isFocusedProvider = FutureProvider.autoDispose<bool>((ref) async {
+  final focusNode = ref.watch(focusNodeProvider);
+  return focusNode.hasFocus;
+});
 
-      node.addListener(listener);
-      return () => node.removeListener(listener);
+bool useIsFocused(FocusNode node, WidgetRef context) {
+  final isFocusedAsyncValue = context.read(isFocusedProvider);
+
+  return isFocusedAsyncValue.when(
+    data: (isFocused) {
+      return isFocused;
     },
-    [node],
+    loading: () => false,
+    error: (_, __) => false,
   );
-
-  return isFocused.value;
 }
